91a93998d3a655d1386153bc1f4f3ed6f0c23135
Benjamin Walkenhorst <krylon@posteo.de>
2025-06-12 22:39:52 +0200

    Start thinking about doing zone transfers concurrently

--- xfr.py
+++ xfr.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2025-06-12 18:22:07 krylon>
+# Time-stamp: <2025-06-12 22:30:33 krylon>
 #
 # /data/code/python/pykuang/xfr.py
 # created on 12. 06. 2025
@@ -19,6 +19,7 @@ pykuang.xfr
 
 import logging
 from queue import Queue
+from threading import Lock, local
 
 from pykuang import common
 from pykuang.database import Database
@@ -29,18 +30,47 @@ class XFRClient:
 
     __slots__ = [
         "log",
-        "db",
+        "loc",
         "queue",
+        "lock",
+        "_active",
     ]
 
     log: logging.Logger
-    db: Database
+    loc: local
     queue: Queue
+    lock: Lock
+    _active: bool
 
     def __init__(self) -> None:
         self.log = common.get_logger("xfr")
-        self.db = Database()
+        self.loc = local()
         self.queue = Queue()
+        self.lock = Lock()
+        self._active = False
+
+    @property
+    def db(self) -> Database:
+        """Return a thread-local database connection."""
+        try:
+            return self.loc.db
+        except AttributeError:
+            self.loc.db = Database()
+            return self.loc.db
+
+    @property
+    def active(self) -> bool:
+        """Return the Client's active flag."""
+        with self.lock:
+            return self._active
+
+    def _run(self) -> None:
+        while self.active:
+            zone: str = self.queue.get()
+            with self.db:
+                xfr = self.db.xfr_add(zone)
+                assert xfr is not None
+                self.log.debug("About to start XFRing zone %s (%d)", xfr.zone, xfr.xid)
 
 
 # Local Variables: #
