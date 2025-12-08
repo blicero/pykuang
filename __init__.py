6e1f4df4c9c328969de2f83013920e1fa1af78e6
Benjamin Walkenhorst <krylon@posteo.de>
2025-12-08 16:29:38 +0100

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2025-12-06 17:55:58 krylon>
+#
+# /data/code/python/pykuang/__init__.py
+# created on 06. 12. 2025
+# (c) 2025 Benjamin Walkenhorst
+#
+# This file is part of the PyKuang network scanner. It is distributed under the
+# terms of the GNU General Public License 3. See the file LICENSE for details
+# or find a copy online at https://www.gnu.org/licenses/gpl-3.0
+
+"""
+pykuang.__init__
+
+(c) 2025 Benjamin Walkenhorst
+"""
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
