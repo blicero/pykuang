4bcb8d5c92262474957526a5e111c3034c3a6332
Benjamin Walkenhorst <krylon@posteo.de>
2025-12-09 15:51:36 +0100

    blacklist - add rudimentary test case for IPBlacklist

--- test_blacklist.py
+++ test_blacklist.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2025-12-09 15:35:41 krylon>
+# Time-stamp: <2025-12-09 15:50:13 krylon>
 #
 # /data/code/python/pykuang/test_blacklist.py
 # created on 09. 12. 2025
@@ -20,10 +20,12 @@ import os
 import re
 import unittest
 from datetime import datetime
+from ipaddress import ip_address, ip_network
 from typing import Final, Optional
 
 from pykuang import common
-from pykuang.blacklist import NameBlacklist, NameBlacklistItem
+from pykuang.blacklist import (IPBlacklist, IPBlacklistItem, NameBlacklist,
+                               NameBlacklistItem)
 
 test_dir: Final[str] = os.path.join(
     "/tmp",
@@ -60,7 +62,7 @@ class TestNameBlacklist(unittest.TestCase):
 
         self.bl(NameBlacklist(patterns=items))
 
-    def test_02_match(self) -> None:
+    def test_02_match_name(self) -> None:
         """Test matching hostnames against the NameBlacklist."""
         test_cases: Final[list[tuple[str, bool]]] = [
             ("www01.funny-domain.com", False),
@@ -74,6 +76,64 @@ class TestNameBlacklist(unittest.TestCase):
         for c in test_cases:
             m: bool = bl.is_match(c[0])
             self.assertEqual(m, c[1])
+
+
+class TestIPBlacklist(unittest.TestCase):
+    """The the IPBlacklist."""
+
+    _bl: IPBlacklist
+
+    @classmethod
+    def bl(cls, bl: Optional[IPBlacklist] = None) -> IPBlacklist:
+        """Get or set the IPBlacklist instance."""
+        if bl is not None:
+            cls._bl = bl
+
+        if cls._bl is not None:
+            return cls._bl
+
+        raise ValueError("IPBlacklist instance is None.")
+
+    def test_01_create_blacklist(self) -> None:
+        """Test creating the IPBlacklist."""
+        ranges: Final[list[str]] = [
+            "0.0.0.0/8",
+            "10.0.0.0/8",
+            "127.0.0.0/8",
+            "169.254.0.0/16",
+            "172.16.0.0/12",
+            "192.0.2.0/24",
+            "192.88.99.0/24",
+            "192.168.0.0/16",
+            "198.18.0.0/15",
+            "198.51.100.0/24",
+            "203.0.113.0/24",
+            "224.0.0.0/4",
+            "240.0.0.0/4",
+            "255.0.0.0/8",
+        ]
+
+        items: Final[list[IPBlacklistItem]] = \
+            [IPBlacklistItem(net=ip_network(x)) for x in ranges]
+
+        bl: Final[IPBlacklist] = IPBlacklist(networks=items)
+        self.bl(bl)
+
+    def test_02_match_addr(self) -> None:
+        """Test matching IP addresses against the IPBlacklist."""
+        test_cases: Final[list[tuple[str, bool]]] = [
+            ("131.24.19.81", False),
+            ("10.10.8.1", True),
+            ("fe80::dead:beef", False),
+        ]
+
+        bl: Final[IPBlacklist] = self.bl()
+
+        for c in test_cases:
+            addr = ip_address(c[0])
+            m: bool = bl.is_match(addr)
+            self.assertEqual(m, c[1])
+
 
 # Local Variables: #
 # python-indent: 4 #
