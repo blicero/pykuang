08520376500d05d228a642e4f34b0abafcab18d3
Benjamin Walkenhorst <krylon@posteo.de>
2025-12-25 22:25:38 +0100

    nexus - start fleshing out API

--- nexus.py
+++ nexus.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2025-12-24 15:17:45 krylon>
+# Time-stamp: <2025-12-25 18:20:50 krylon>
 #
 # /data/code/python/pykuang/nexus.py
 # created on 11. 12. 2025
@@ -18,10 +18,13 @@ pykuang.nexus
 
 import logging
 from dataclasses import dataclass, field
+from queue import Queue
 from threading import RLock
 
 from pykuang import common
+from pykuang.control import Facility, Message
 from pykuang.generator import ParallelGenerator
+from pykuang.xfr import XFRProcessor
 
 
 @dataclass(kw_only=True, slots=True)
@@ -31,13 +34,17 @@ class Nexus:
     log: logging.Logger = field(default_factory=lambda: common.get_logger("nexus"))
     lock: RLock = field(default_factory=RLock)
     _active: bool = False
+    cmdQ: Queue[Message] = field(init=False)
     pgen: ParallelGenerator = field(init=False)
+    pxfr: XFRProcessor = field(init=False)
     gcnt: int
     xcnt: int
     scnt: int
 
     def __post_init__(self) -> None:
+        self.cmdQ = Queue()
         self.pgen = ParallelGenerator(wcnt=self.gcnt)
+        self.pxfr = XFRProcessor(wcnt=self.xcnt)
 
     @property
     def active(self) -> bool:
@@ -47,8 +54,32 @@ class Nexus:
 
     def start(self) -> None:
         """Let get this Nexus started!"""
+        self.pxfr.start()
         self.pgen.start()
 
+    def start_one(self, subsystem: Facility) -> None:
+        """Start a single worker thread in the specified subsystem."""
+        self.log.debug("Start one %s thread.",
+                       subsystem.name)
+        match subsystem:
+            case Facility.Generator:
+                self.pgen.start_one()
+            case Facility.XFR:
+                self.pxfr.start_one()
+            case Facility.Scanner:
+                self.log.error("IMPLEMENTME - Scanner is not implemented, yet!!!")
+
+    def stop_one(self, subsystem: Facility) -> None:
+        """Stop one worker thread in the specified subsystem."""
+        self.log.debug("Stop one %s thread.",
+                       subsystem.name)
+        match subsystem:
+            case Facility.Generator:
+                self.pgen.stop_one()
+            case Facility.XFR:
+                self.pxfr.stop_one()
+            case Facility.Scanner:
+                self.log.error("IMPLEMENTME - Scanner is not implemented, yet!!!")
 
 # Local Variables: #
 # python-indent: 4 #
