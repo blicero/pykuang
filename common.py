a516dcca08a74f0799c4052437cfb0bb26950543
Benjamin Walkenhorst <krylon@posteo.de>
2026-01-05 16:16:26 +0100

    common - start setting per-module log levels

--- common.py
+++ common.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Time-stamp: <2025-12-20 16:12:09 krylon>
+# Time-stamp: <2026-01-05 16:15:28 krylon>
 #
 # /data/code/python/headlines/src/headlines/common.py
 # created on 30. 09. 2025
@@ -21,6 +21,7 @@ import logging.handlers
 import os
 import pathlib
 import sys
+from collections import defaultdict
 from datetime import datetime, timedelta
 from threading import Lock
 from typing import Final, Optional
@@ -31,6 +32,10 @@ Debug: Final[bool] = True
 TimeFmt: Final[str] = "%Y-%m-%d %H:%M:%S"
 
 log_level_tty: int = logging.DEBUG
+
+log_levels: Final[defaultdict[str, int]] = defaultdict(lambda: logging.DEBUG)
+
+log_levels["generator"] = logging.INFO
 
 
 def parse_iso_date(s: str, end: bool = False) -> Optional[datetime]:
@@ -131,8 +136,10 @@ def get_logger(name: str, terminal: bool = True) -> logging.Logger:
         max_log_size = 4 * 2**20  # 4 MiB
         max_log_count = 10
 
+        lvl = log_levels[name]
+
         log_obj = logging.getLogger(name)
-        log_obj.setLevel(logging.DEBUG)
+        log_obj.setLevel(lvl)
         log_file_handler = logging.handlers.RotatingFileHandler(path.log,
                                                                 'a',
                                                                 max_log_size,
